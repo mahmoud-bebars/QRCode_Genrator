c9bd0446afeca7bad896998077d6705af70c6822
m.bebars <66588352+mahmoud-bebars@users.noreply.github.com>
2024-06-24 16:50:21 +0300

    (refactor): fixing styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,8 @@ const Home = () => {
   return (
     <>
       <main className="grid h-screen w-screen place-items-center bg-white bg-cover bg-[url('https://images.unsplash.com/photo-1550482768-88b710a445fd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80')] ">
+        <QrCodeCard />{" "}
         <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-t from-black/80 via-black/50" />
-        <QrCodeCard />
       </main>
     </>
   );
